9dc175dde6dd4793349be5db3df72e5d2f399d4c
AveraBell <avera.bell@gmail.com>
2015-12-09 23:43:09 +0400

    Create rosalind_REVC.py

---
+++ rosalind_REVC.py
@@ -0,0 +1,21 @@
+a=input()
+k=len(a)-1
+for i in range(0,len(a)):
+    if a[i]=="A":
+        a=a.replace('A','B')
+for i in range(0,len(a)):
+    if a[i]=="T":
+        a=a.replace('T','A')
+for i in range(0,len(a)):
+    if a[i]=="G":
+        a=a.replace('G','D')
+for i in range(0,len(a)):
+    if a[i]=="C":
+        a=a.replace('C','G')
+for x in range(0,len(a)):
+    if a[x]=="B":
+        a=a.replace('B','T')
+for x in range(0,len(a)):
+    if a[x]=="D":
+        a=a.replace('D','C')
+print (a)
