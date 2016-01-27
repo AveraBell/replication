68ef00e1b94646478f299c095987263fd44c4220
AveraBell <avera.bell@gmail.com>
2016-01-27 10:08:05 +0300

    Create rosalind_GC.py

---
+++ rosalind_GC.py
@@ -0,0 +1,25 @@
+for i in range (0, 1):
+    a=input()
+    b=input()
+    c=0
+    v=0
+    for x in range (0, len(b)):
+        l='mn'
+        if b[x]=='C':
+            c=c+1
+    for s in range (0, len(b)):
+        f=0
+        if b[s]=='G':
+            f=f+1
+    e=len(b)
+    g=c+f
+    d=g/e
+    if d>v:
+        v=d
+        l=a
+z=v*100
+print (c)
+print (f)
+print (g)
+print (l)
+print (z)
